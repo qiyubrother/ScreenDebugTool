de95e9c20a86d8b02d978a766a6592daa7c37385
qiyubrother <qiyubrother@outlook.com>
2019-10-22 15:51:42 +0800

    添加项目文件。

---
+++ ScreenDebug/FrmMain.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ScreenDebug
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btnGetDeviceName = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetDeviceName
+            // 
+            this.btnGetDeviceName.Location = new System.Drawing.Point(15, 18);
+            this.btnGetDeviceName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGetDeviceName.Name = "btnGetDeviceName";
+            this.btnGetDeviceName.Size = new System.Drawing.Size(212, 33);
+            this.btnGetDeviceName.TabIndex = 0;
+            this.btnGetDeviceName.Text = "Get Screen Information";
+            this.btnGetDeviceName.UseVisualStyleBackColor = true;
+            this.btnGetDeviceName.Click += new System.EventHandler(this.btnGetDeviceName_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(15, 61);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(903, 480);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(233, 18);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(87, 33);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(831, 18);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(87, 33);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(933, 561);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnGetDeviceName);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Screen Debug Tool";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetDeviceName;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ ScreenDebug/FrmMain.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenDebug
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void btnGetDeviceName_Click(object sender, EventArgs e)
+        {
+            var sm = new ScreenManager();
+            sm.Init();
+            if (listBox1.Items.Count != 0)
+            {
+                listBox1.Items.Add(Environment.NewLine);
+            }
+            foreach (var s in sm.ScreensInfo)
+            {
+                listBox1.Items.Add($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff")}]{s.ToString()}");
+            }
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+    public class ScreenManager
+    {
+        private List<ScreenInfo> _lst = new List<ScreenInfo>();
+        public List<ScreenInfo> GetRuntimeScreenConfig()
+        {
+            var lst = new List<ScreenInfo>();
+            for (var i = 0; i < Screen.AllScreens.Length; i++)
+            {
+                lst.Add(new ScreenInfo
+                {
+                    ScreenNo = i,
+                    DeviceName = Screen.AllScreens[i].DeviceName,
+                    IsPrimaryScreen = Screen.AllScreens[i].Primary
+                });
+            }
+
+            return lst;
+        }
+
+        public void Init()
+        {
+            _lst = GetRuntimeScreenConfig();
+        }
+
+        /// <summary>
+        /// 检测运行时，屏幕配置是否有变化
+        /// </summary>
+        /// <returns></returns>
+        public bool IsRuntimeScreenConfig()
+        {
+            var lstRuntime = GetRuntimeScreenConfig();
+            if (_lst.Count != lstRuntime.Count)
+            {
+                return true;
+            }
+            foreach (var s in _lst)
+            {
+                if (!lstRuntime.Any(x => x.ScreenNo == s.ScreenNo && x.DeviceName == s.DeviceName))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 返回控件所在的屏幕索引
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public int GetScreenNoByControl(Control c)
+        {
+            // 根据当前组件的位置查找组件所在屏幕的索引
+            var sc = _lst.FirstOrDefault(x => x.DeviceName == Screen.FromControl(c).DeviceName);
+            if (sc != null)
+            {
+                return sc.ScreenNo;
+            }
+            else
+            {
+                // 未找到，需要重新扫描显示器配置
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            _lst.Sort(new ScreenInfoCompare());
+            for (var i = 0; i < _lst.Count; i++)
+            {
+                sb.AppendLine(_lst[i].ToString());
+            }
+            return sb.ToString();
+        }
+
+        public IEnumerable<ScreenInfo> ScreensInfo => _lst;
+
+        private class ScreenInfoCompare : IComparer<ScreenInfo>
+        {
+            public int Compare(ScreenInfo x, ScreenInfo y)
+            {
+                if (x.ScreenNo < y.ScreenNo)
+                    return -1;
+                else if (x.ScreenNo == y.ScreenNo)
+                    return 0;
+                else
+                    return 1;
+            }
+        }
+    }
+
+    public class ScreenInfo
+    {
+        /// <summary>
+        /// 显示器的设备名
+        /// </summary>
+        public string DeviceName { get; set; }
+        /// <summary>
+        /// 显示器编号
+        /// </summary>
+        public int ScreenNo { get; set; }
+        /// <summary>
+        /// 是否是默认显示器
+        /// </summary>
+        public bool IsPrimaryScreen { get; set; }
+
+        public override string ToString()
+        {
+            return $"ScreenNo={ScreenNo}, DeviceName={DeviceName}, IsPrimaryScreen={IsPrimaryScreen}";
+        }
+    }
+}
